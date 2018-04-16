a871d5d9d52a504bd02fbaa12c758b5841729254
Alvin Go <alvingogo25@gmail.com>
2018-04-15 23:26:16 -0700

    added relative path

--- src/components/ImgCard.js
+++ src/components/ImgCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ImgCard = props => (
 <div className="tile">
-  <img id={props.id} alt={props.name} src={`/images/${props.name}.png`} onClick={() => props.setClicked(props.id)} className="img"/>
+  <img id={props.id} alt={props.name} src={`./images/${props.name}.png`} onClick={() => props.setClicked(props.id)} className="img"/>
 </div>
 );
 
